1d5931e8306bb2b6f4bf2d7cf34f6dfb724bfccb
andrewhun <andrewhun995@gmail.com>
2025-02-17 22:05:34 +0000

    Adding Basic App Test

---
+++ src/test/java/com/andrewhun/finance/AppTest.java
@@ -0,0 +1,31 @@
+package com.andrewhun.finance;
+
+import javafx.stage.Stage;
+import org.junit.jupiter.api.*;
+import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.api.FxToolkit;
+
+public class AppTest extends ApplicationTest {
+
+    protected Stage stage;
+
+
+    @BeforeAll
+    static void setUpHeadlessMode() {
+
+        System.setProperty("testfx.robot", "glass");
+        System.setProperty("testfx.headless", "true");
+        System.setProperty("prism.order", "sw");
+        System.setProperty("prism.text", "t2k");
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        this.stage = stage;
+        new App().start(stage);
+    }
+
+    @Test
+    void testDisplayingHelloWorldPage() {
+        Assertions.assertEquals("Hello World!", stage.getTitle());
+    }
+}
